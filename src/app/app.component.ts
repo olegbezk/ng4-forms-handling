cd0b986330c5d3bdd08bb4fc8a3bffae69a93b5d
oleg.bezkorovaynyi <oleg.bezkororvaynyi@globallogic.com>
2017-05-28 18:43:58 +0300

    setting and patching form values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,27 @@ import {NgForm} from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('f') signUp: NgForm;
+  @ViewChild('f') signupForm: NgForm;
   defaultQuestion = 'teacher';
   answer = '';
   genders = ['male', 'female'];
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
   // onSubmit(form: NgForm) {
@@ -21,6 +35,6 @@ export class AppComponent {
   // }
 
   onSubmit() {
-    console.log(this.signUp);
+    console.log(this.signupForm);
   }
 }
